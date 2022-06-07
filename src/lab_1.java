7d3db91e7d39212a606dad4b0682dc8742850c0d
S-B-A <sasha.berg.agency@gmail.com>
2022-06-07 10:41:52 +0300

    Initial commit

---
+++ src/lab_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class lab_1 {
+    public static void main(String args[]) {
+        System.out.println("Программа подсчёта суммы трёх чисел");
+        System.out.println("Введите первое число:");
+        Scanner scan1 = new Scanner(System.in);
+        int number1 = scan1.nextInt();
+        System.out.println("Введите второе число");
+        Scanner scan2 = new Scanner(System.in);
+        int number2 = scan2.nextInt();
+        int sum2 = number1 + number2;
+
+        System.out.println("Введите третье число");
+        Scanner scan3 = new Scanner(System.in);
+        int number3 = scan3.nextInt();
+        int result = sum2 + number3;
+
+        System.out.printf("Сумма чисел = %d%n", result);
+    }
+}
