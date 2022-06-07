5b3c329c445203146b7c8958a4d0f47685db4008
S-B-A <sasha.berg.agency@gmail.com>
2022-06-07 17:13:39 +0300

    Таблица умножния на 7ю

---
+++ src/lab_mass_cycle_2.java
@@ -0,0 +1,8 @@
+public class lab_mass_cycle_2 {
+    public static void main(String[] args ) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + " * 7 = " + ( 7 * i ));
+        }
+
+    }
+}
